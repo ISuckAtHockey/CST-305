af66388a5c929b889548ac036e2775085c4138cd
ISuckAtHockey <108446124+ISuckAtHockey@users.noreply.github.com>
2023-10-01 23:17:36 -0700

    Add files via upload

--- driver.py
+++ driver.py
@@ -1,64 +1,53 @@
-#Ethan Atkins
-#Using numpy, scipy, matplotlib
-#Approach: Once I had created the differential equation I coded it into this program, added the user inputs for initial temperatures, solved the equations using the included packages, and plotted the solutions.
-
+#!/usr/bin/env python
+# Ethan Atkins
+# Using NumPy, SciPy, matplotlib
+# Approach:
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
-# Define the ODE for CPU temperature
-def cpu_temperature(t, T):
-    P = 10.0  # Power consumption (in watts)
-    k = 0.1   # Heat dissipation rate constant
-    Tambient = 25.0  # Ambient temperature (in degrees Celsius)
-    C = 1.0   # Heat capacity (in joules per degree Celsius)
+def ODE(x, y):
+    return -(y) + np.log(x)
 
-    dTdt = (P - k * (T - Tambient)) / C
-    return dTdt
+def RKF(x, y, h):
+    newX, newY = 0.0, 0.0
+    k = ODE(x, y)
+    kk = ODE(x + h/2, y + ((h * k)/2))
+    kkk = ODE(x + h/2, y + ((h * kk)/2))
+    kkkk = ODE(x + h, y + ((h * kkk)))
+    newY = y + (h * (1/6) * (k + (2 * kk) + (2 * kkk) + kkkk))
+    newX = x + h
+    return newX, newY
 
-# Define the time span for the integration
-t_span = (0, 10)  # Start at t=0 and integrate up to t=10 seconds
+n, x, y, h = 0, 2.0, 1.0, 0.3
+results = []
+xR = []
+yR = []
 
-# Allow user to define the initial temperatures
-initial_temperature1 = [input("Please enter an initial temperature in degrees Celsius: ")]
-initial_temperature2 = [input("\nPlease enter an initial temperature in degrees Celsius: ")]
-initial_temperature3 = [input("\nPlease enter an initial temperature in degrees Celsius: ")]
-initial_temperature4 = [input("\nPlease enter an initial temperature in degrees Celsius: ")]
-initial_temperature5 = [input("\nPlease enter an initial temperature in degrees Celsius: ")]
+while n <= 1000:
+    results.append([n, x, y, ODE(x, y)])
+    xR.append(x)
+    yR.append(y)
+    r = RKF(x, y, h)
+    x, y = r[0], r[1]
+    n += 1
 
-# Solve the ODEs
-solution1 = solve_ivp(cpu_temperature, t_span, initial_temperature1, t_eval=np.linspace(t_span[0], t_span[1], 100))
-solution2 = solve_ivp(cpu_temperature, t_span, initial_temperature2, t_eval=np.linspace(t_span[0], t_span[1], 100))
-solution3 = solve_ivp(cpu_temperature, t_span, initial_temperature3, t_eval=np.linspace(t_span[0], t_span[1], 100))
-solution4 = solve_ivp(cpu_temperature, t_span, initial_temperature4, t_eval=np.linspace(t_span[0], t_span[1], 100))
-solution5 = solve_ivp(cpu_temperature, t_span, initial_temperature5, t_eval=np.linspace(t_span[0], t_span[1], 100))
+for r in results:
+    print(f"--------------------{r[0]}--------------------\n")
+    print(f"x{r[0]} = {r[1]}")
+    print(f"y{r[0]} = {r[2]}")
+    print(f"Solution = {r[3]}\n")
 
-# Extract the results
-t1 = solution1.t
-T1 = solution1.y[0]
+t_span = (1, 300)
+ini_con = [0]
+sol = solve_ivp(ODE, t_span, ini_con, t_eval=np.linspace(1, 300, 300))
+solT = sol.t
+solY = sol.y[0]
 
-t2 = solution2.t
-T2 = solution2.y[0]
-
-t3 = solution3.t
-T3 = solution3.y[0]
-
-t4 = solution4.t
-T4 = solution4.y[0]
-
-t5 = solution5.t
-T5 = solution5.y[0]
-
-# Plot the results
-plt.figure(figsize=(10, 6))
-plt.plot(t1, T1, label='CPU Temperature (' + initial_temperature1[0] + '°C)')
-plt.plot(t2, T2, label='CPU Temperature (' + initial_temperature2[0] + '°C)')
-plt.plot(t3, T3, label='CPU Temperature (' + initial_temperature3[0] + '°C)')
-plt.plot(t4, T4, label='CPU Temperature (' + initial_temperature4[0] + '°C)')
-plt.plot(t5, T5, label='CPU Temperature (' + initial_temperature5[0] + '°C)')
-plt.xlabel('Time (s)')
-plt.ylabel('Temperature (°C)')
+plt.figure(figsize = (10,6))
+plt.plot(xR, yR, 'bo-', label = 'RKF')
+plt.plot(solT, solY, 'g^-', label = 'solve_ivp')
+plt.title('-y + ln(x)')
 plt.legend()
-plt.title('CPU Temperature Over Time')
 plt.grid(True)
 plt.show()
